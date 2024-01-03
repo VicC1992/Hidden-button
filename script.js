19cbaccd5887da46eb37abdc2e3293a1fa113144
VicC1992 <151568006+VicC1992@users.noreply.github.com>
2024-01-03 20:52:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
+let numberOfButtons = 3;
 const placeAddButton = document.getElementById("buttonContainer");
 const generateButtons = document.getElementById("createButtons");
-let buttons = document.querySelectorAll(".btn");
 
-function winButton() {
-    let x = Math.floor((Math.random() * 2) + 1);
-    if (x === 1) {
-        alert("Win!!");
-    } else {
-        alert("Lose!!");
-    }
-}
-for(let i = 0; i < buttons.length; ++i) {
-    buttons[i].addEventListener("click", winButton);
-}
-
-function storeValue() {
-    numberOfButtons = document.getElementById("number").value;
-}
-generateButtons.addEventListener("click", storeValue);
+window.onload = createButtons;
 
 function createButtons() {
-    if(numberOfButtons == 2 ) {
-        document.getElementById("button3").remove();
-
-    } else if (numberOfButtons > 3) {
-    for(let i = 0; i < numberOfButtons - 3; ++i) {
-        const newBtn = document.createElement("button");
-        newBtn.textContent = "Click";
-        newBtn.className= "btn btn-primary";
-        newBtn.addEventListener("click", winButton);
-        placeAddButton.appendChild(newBtn);
+    winIdButton = Math.floor((Math.random() * numberOfButtons) + 1);
+    let idBtns = 0;
+    if (numberOfButtons === 3) {
+        for (let i = 0; i < numberOfButtons; ++i) {
+            ++idBtns;
+            const newBtn = document.createElement("button");
+            newBtn.textContent = "Click";
+            newBtn.className = "btn btn-primary";
+            newBtn.id = idBtns;
+            newBtn.addEventListener("click", winOrLose);
+            placeAddButton.appendChild(newBtn);
+        }
+    } else if (numberOfButtons == 2) {
+        document.getElementById("3").remove();
+    } else if(numberOfButtons > 3) {
+        idBtns = 3;
+        for (let i = 0; i < numberOfButtons - 3; ++i) {
+            ++idBtns;
+            const newBtn = document.createElement("button");
+            newBtn.textContent = "Click";
+            newBtn.className = "btn btn-primary";
+            newBtn.id = idBtns;
+            newBtn.addEventListener("click", winOrLose);
+            placeAddButton.appendChild(newBtn);
         }
     }
 }
+
+function winOrLose(e) {
+    let pressedButton = e.target.id;
+    if (winIdButton == pressedButton) {
+        alert("Win !!!");
+    } else {
+        alert("Lose !!!");
+    }
+}
+
+function sotreInputValue() {
+    numberOfButtons = document.getElementById("inputNumber").value;
+}
+generateButtons.addEventListener("click", sotreInputValue);
 generateButtons.addEventListener("click", createButtons);
